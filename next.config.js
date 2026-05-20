9958fcbffea9803f5bfe5e01a9b0bfd399111b13
Takeo Fujimoto <llmlabo2019@gmail.com>
2026-05-20 16:31:34 +0900

    Fix:Modified related files for reading outputs_json again

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   env: {
     BASE_URL: process.env.BASE_URL,
     ORIGIN: process.env.ORIGIN,
+    USER_POOL_ID: process.env.USER_POOL_ID,
+    CLIENT_ID: process.env.CLIENT_ID,
   },
 };
 
